916b1ff06b7fbead2347c59d33d5012fe5b58a7a
Shawn Sudaria <shawnlovecode14@gmail.com>
2025-06-16 19:34:01 +0800

    Set up file upload infrastructure: added media settings, updated Book model, created upload form, view, template, and initial migration for creative_works app.

--- Hackathon/urls.py
+++ Hackathon/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('creative_works/', include('creative_works.urls')),
     path('event_planner/', include('event_planner.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
