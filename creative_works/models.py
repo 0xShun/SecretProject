b48d43716e7d79fd3d772f4eebf17a1df8e597fe
Josephe-Anne Marie Misola <120217663+caesar0041@users.noreply.github.com>
2025-06-16 19:34:07 +0800

    better sidebar (white bg)

--- creative_works/models.py
+++ creative_works/models.py
@@ -1,31 +1,31 @@
-# from django.db import models
-# from accounts import models as accounts_model
+from django.db import models
+from accounts import models as accounts_model
 
-# class Genre(models.Model):
-#     genre = models.CharField(max_length=100, unique=True)
+class Genre(models.Model):
+    genre = models.CharField(max_length=100, unique=True)
 
-#     def __str__(self):
-#         return self.genre
-
-
-# class Book(models.Model):
-#     title = models.CharField(max_length=255)
-#     author = models.CharField(max_length=255)
-#     uploader = models.ForeignKey(accounts_model.UserProfile, on_delete=models.CASCADE)
-#     upload_date = models.DateTimeField(auto_now_add=True)
-#     description = models.TextField()
-#     content = models.TextField()
-
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.genre
 
 
-# class GenreBook(models.Model):
-#     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-#     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    uploader = models.ForeignKey(accounts_model.UserProfile, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    content = models.TextField()
 
-#     class Meta:
-#         unique_together = ('book', 'genre')
+    def __str__(self):
+        return self.title
 
-#     def __str__(self):
-#         return f"{self.book.title} - {self.genre.genre}"
+
+class GenreBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('book', 'genre')
+
+    def __str__(self):
+        return f"{self.book.title} - {self.genre.genre}"
