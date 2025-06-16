da4a97d972192a49cd6dd3bdc8f900020a1fda20
0x13 <dilucrag19@gmail.com>
2025-06-16 14:11:26 +0800

    Create a UserProfile and UserCredential model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    birthdate = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
+
+
+class UserCredential(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_profile')
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    user_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.email}"
