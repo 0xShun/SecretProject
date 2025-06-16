ed65d4541131489ef508bb6165dffa4190b5ae49
Shawn Sudaria <shawnlovecode14@gmail.com>
2025-06-16 19:07:40 +0800

    Added view book detail page

--- creative_works/urls.py
+++ creative_works/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.creatives_home, name='creatives_home'),
     path('upload_book', views.upload_book, name='upload_book'),
-    path('book_page', views.book_page, name='book_page')
+    path('book_page', views.book_page, name='book_page'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
