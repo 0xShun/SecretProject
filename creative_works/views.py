fa2f5bf1bf493516d689c3321786dfc6e8abace0
comradeZnev <venzmilado@gmail.com>
2025-06-16 18:29:09 +0800

    creative_works templates, views, and urls

--- creative_works/views.py
+++ creative_works/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def creatives_home(request):
+
+    return render(request, 'creative_home.html')
+
+def upload_book(request):
+
+    return render(request, 'upload_book.html')
+
+def book_page(request):
+
+    return render(request, 'book_page.html')
