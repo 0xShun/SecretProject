0e89b5cc2540c7b5faf4fd31c57d89e1cf652811
0x13 <dilucrag19@gmail.com>
2025-06-16 18:32:21 +0800

    added a user auth and user authorization

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,21 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
     birthdate = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def age(self):
+        today = date.today()
+        year = today.year - self.birthdate.year
+        cmp = ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+        return year - cmp
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name} {self.last_name}"
