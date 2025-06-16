368289241c90b570d1f7740772ac0cad2e576873
Lanxxxe <Lanxxxe>
2025-06-16 16:22:50 +0800

    create: Event home page and sidebar

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from . import views
+
+
 urlpatterns = [
-    
+    path('', views.event_dashboard, name='event_dashboard'),
+    path('home', views.event_home, name='event_home'),
 ]
--- event_planner/views.py
+++ event_planner/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def event_dashboard(request):
+
+    return render(request, 'event_base.html')
+
+def event_home(request):
+
+    return render(request, 'event_home.html')
